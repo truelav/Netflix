9a557f40003b24d1e5cd6da0091d9825f554a1dd
truelav <madan.serghei@yahoo.com>
2020-12-23 10:51:57 -0800

    adding dependencies

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h2>This is the App</h2>
+    </div>
+  );
+}
